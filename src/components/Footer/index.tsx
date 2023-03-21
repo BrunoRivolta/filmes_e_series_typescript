9c858a7e63bf695d1048befe1c2536f90319bd80
Bruno Rivolta <brrivolta@gmail.com>
2023-03-21 15:44:19 -0300

    Adicionado footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Footer.module.scss'
+
+export default function Footer() {
+  return (
+    <section className={styles.footer}>
+        <p>Bruno Rivolta</p>
+    </section>
+  )
+}
--- src/pages/StandatdPage/index.tsx
+++ src/pages/StandatdPage/index.tsx
@@ -1,3 +1,4 @@
+import Footer from 'components/Footer'
 import Header from 'components/Header'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
@@ -7,6 +8,7 @@ export default function StandardPage() {
     <main>
       <Header />
       <Outlet />
+      <Footer />
     </main>
   )
 }
